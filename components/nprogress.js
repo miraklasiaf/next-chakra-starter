6d12dc58b64473fa5cff560dca3ddb6f09f3fa8f
Faisal Karim <faisalkarim96@gmail.com>
2020-08-21 09:06:36 +0700

    Remove ga

--- components/nprogress.js
+++ components/nprogress.js
@@ -1,23 +1,22 @@
-import Router from 'next/router'
-import nprogress from 'nprogress'
-import debounce from 'lodash.debounce'
+import Router from 'next/router';
+import nprogress from 'nprogress';
+import debounce from 'lodash.debounce';
 
 // Only show nprogress after 500ms (slow loading)
-const start = debounce(nprogress.start, 500)
+const start = debounce(nprogress.start, 500);
 
-Router.events.on('routeChangeStart', start)
+Router.events.on('routeChangeStart', start);
 
 Router.events.on('routeChangeComplete', (url) => {
-  start.cancel()
-  nprogress.done()
-  window.scrollTo(0, 0)
-  gtag.pageview(url)
-})
+  start.cancel();
+  nprogress.done();
+  window.scrollTo(0, 0);
+});
 
 Router.events.on('routeChangeError', () => {
-  start.cancel()
-  nprogress.done()
-})
+  start.cancel();
+  nprogress.done();
+});
 
 const Nprogress = () => (
   <style jsx global>
@@ -53,6 +52,6 @@ const Nprogress = () => (
       }
     `}
   </style>
-)
+);
 
-export default Nprogress
+export default Nprogress;
